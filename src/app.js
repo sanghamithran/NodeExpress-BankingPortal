df8b8ed7b3128c153e77f403dc36918a28954e7d
Sanghamithran S <sanghamithran@gmail.com>
2020-01-09 17:32:02 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const express =  require('express');
+const port = 3000
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.render('index', {title:'Index'});
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
