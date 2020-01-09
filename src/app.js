97dd6d08affabd03e748a730818f5ac74a1c1de9
Sanghamithran S <sanghamithran@gmail.com>
2020-01-09 17:38:29 +0530

    public dir fix 2

--- src/app.js
+++ src/app.js
@@ -7,10 +7,9 @@ const port = 3000
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
     res.render('index', {title:'Index'});
